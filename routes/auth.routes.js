58f0a5f6044c96bc029ac206ef6615ec13d099d5
Rieko00 <ariekbudianto@gmail.com>
2025-05-28 16:44:08 +0700

    Fix issue

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
-const profileController = require("../controllers/profile.controller")
+const profileController = require("../controllers/profile.controller");
 const { validateRegister, validateLogin, validateForgotPassword, validateVerifyForgotPassword, validateRiwayatToken, validateCodeQuery } = require("../middleware/validation.middleware");
 const { authenticateToken, validateUpdateProfile } = require("../middleware/auth.middleware");
 
 // Auth routes
 router.post("/register", validateRegister, authController.register);
 router.post("/login", validateLogin, authController.login);
-router.post("/verify-token", authController.verifyToken);
+router.get("/verify-token", authController.verifyToken);
 router.post("/forgot-password", validateForgotPassword, authController.forgotPassword);
 router.patch("/forgot-password/verify", validateCodeQuery, validateVerifyForgotPassword, authController.verifyForgotPassword);
 router.patch("/profile/update", authenticateToken, validateUpdateProfile, profileController.updateProfile);
